c1545da2b9cc78cd03a82a29d3d63778b1bb7d91
GurKalra <gurvanshsinghkalra@gmail.com>
2026-03-03 14:38:12 +0530

    Use Case 2: Basic Room Types & Static Availability

---
+++ src/UseCase2RoomInitialization.java
@@ -0,0 +1,32 @@
+public class UseCase2RoomInitialization {
+
+    public void execute() {
+
+        System.out.println("Hotel Room Initialization");
+        System.out.println();
+
+        // Polymorphic references
+        Room singleRoom = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suiteRoom = new SuiteRoom();
+
+        // Static availability (intentionally simple)
+        int singleAvailability = 5;
+        int doubleAvailability = 3;
+        int suiteAvailability = 2;
+
+        System.out.println("Single Room:");
+        singleRoom.displayRoomDetails();
+        System.out.println("Available: " + singleAvailability);
+        System.out.println();
+
+        System.out.println("Double Room:");
+        doubleRoom.displayRoomDetails();
+        System.out.println("Available: " + doubleAvailability);
+        System.out.println();
+
+        System.out.println("Suite Room:");
+        suiteRoom.displayRoomDetails();
+        System.out.println("Available: " + suiteAvailability);
+    }
+}
